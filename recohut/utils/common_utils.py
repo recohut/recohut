b02af1c95a094dae9b3ba92a435eea99159137d6
sparsh <sparsh.cloud@gmail.com>
2022-01-22 11:31:28 +0000

    build lib

--- recohut/utils/common_utils.py
+++ recohut/utils/common_utils.py
@@ -181,6 +181,9 @@ def seed_everything(seed=40):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    # some cudnn methods can be random even after fixing the seed
+    # unless you tell it to be deterministic
     torch.backends.cudnn.deterministic = True
 
 # Cell
